b5c12a871eda5d5ad4c73058dec72b6701c8fcd7
Abu <efootball042605@gmail.com>
2025-12-18 20:13:16 +0500

    1-mashq

---
+++ practice01.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    result = a + b
+    return result
+
+def subtract(a, b):
+    result = a - b
+    return result
+
+def multiply(a, b):
+    result = a * b
+    return result
+
+def divide(a, b):
+    result = a / b
+    return result
+
+def main():
+    a = int(input('a: '))
+    b = int(input('b: '))
+
+
+    op = input('operator (+, -, *, /): ')
+    if op == '+':
+        result = add(a, b)
+    elif op == '-':
+        result = subtract(a, b)
+    elif op == '*':
+        result = multiply(a, b)
+    elif op == '/':
+        result = divide(a, b)
+
+    print(result)
+
+main()        
